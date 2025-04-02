f16a6b9fdd99867aeb22b2261310ba460722f58b
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-04-02 21:58:49 +0530

    Added mail sender function using Nodemailer

--- models/OTP.js
+++ models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
     email:{
@@ -16,5 +17,20 @@ const OTPSchema = new mongoose.Schema({
     }
 });
 
+// function to send emails
+async function sendVerificationEmail(email, otp) {
+    try {
+        const mailResponse = await mailSender(email, "Verification Email From StudyNotion", otp);
+        console.log("Email sent Successfully: ", mailResponse);
+    } catch(error){
+        console.log("error occured while sending mails: ", error)
+        throw error;
+    }
+}
+
+OTPSchema.pre("save", async function (next) {
+    await sendVerificationEmail(this.email, this.otp);
+    next();    
+})
 
 module.exports = mongoose.model("OTP", OTPSchema);
