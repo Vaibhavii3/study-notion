f7bfef6c7f791cee6dfa9ebef1e01dc323425aa5
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-04-13 23:01:54 +0530

    routes

---
+++ routes/Profile.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+
+const { auth } = require("../middlewares/auth")
+const { deleteAccount,
+        updateDisplayPicture,
+        updateProfile,
+        getAllUserDetails,
+        getEnrolledCourses
+} = require("../controllers/Profile")
+
+
+router.get("/getUserDetails", auth, getAllUserDetails)
+router.put("/updateProfile", auth, updateProfile)
+router.delete("/deleteProfile", deleteAccount)
+
+router.get("/getEnrolledCourses", auth, getEnrolledCourses)
+router.put("/updateDisplayPicture", auth, updateDisplayPicture)
+
+module.exports = router
---
+++ routes/User.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const router = express.Router()
+
+const {
+    login,
+    sendotp,
+    signup,
+    changePassword,
+} = require("../controllers/Auth")
+
+const {
+    resetPassword,
+    resetPasswordToken,
+} = require("../controllers/ResetPassword")
+
+const { auth } = require("../middlewares/auth")
+
+router.post("/login", login)
+router.post("/signup", signup)
+router.post("/sendotp", sendotp)
+router.post("/changepassword", auth, changePassword)
+
+router.post("/reset-password-token", resetPasswordToken)
+
+router.post("/reset-password", resetPassword)
+
+module.exports = router
+
