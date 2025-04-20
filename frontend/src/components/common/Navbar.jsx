3e1fb4097aeceac2154433d6a3f9248e2fb39108
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-04-21 00:56:09 +0530

    navbar

---
+++ frontend/src/components/common/Navbar.jsx
@@ -0,0 +1,144 @@
+import React, { useEffect} from "react";
+import logo from "../../assets/Logo/Logo-Full-Light.png"
+import { Link, matchPath } from 'react-router-dom'
+import {NavbarLinks} from "../../data/navbar-links"
+import { useLocation } from "react-router-dom";
+import { useSelector } from 'react-redux'
+import {AiOutlineShoppingCart} from "react-icons/ai"
+import { apiConnector } from "../../services/apiconnector"
+import { categories } from "../../services/apis"
+import { useState } from 'react'
+import {IOIosArrowDropdownCircle} from "react-icons/io"
+import ProfileDropDown from "../core/Auth/ProfileDropDown";
+
+const subLinks = [
+    {
+        title: "python",
+        link: "/catalog/python"
+    },
+    {
+        title: "web dev",
+        link: "/catalog/web-development"
+    }
+]
+
+const Navbar = () => {
+    console.log("Printing base url:", process.env.REACT_APP_BASE_URL);
+    const {token} = useSelector( (state) => state.auth);
+    const {user} = useSelector( (state) => state.profile);
+    const {totalItems} = useSelector((state) => state.cart)
+    const location = useLocation();
+
+    const [ssubLinks, setSsubLinks] = useState([]);
+
+    const fetchSublinks = async() => {
+        try{
+            const result = await apiConnector("GET", categories.CATEGORIES_API);
+            console.log("Printing Sublinks result:" , result);
+            setSsubLinks(result.data.data);
+        } catch(error) {
+            console.log("Could not fetch the category list");
+        }
+    }
+
+    useEffect( () => {
+        fetchSublinks();
+    }, [])
+
+    const matchRoute = (route) => {
+        return matchPath({path:route}, location.pathname);
+    }
+
+    return (
+        <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
+            <div className="flex w-11/12 max-w-maxContent items-center justify-center">
+
+                <Link to="/">
+                <img src={logo} width={150} height={42} loading="lazy" />
+                </Link>
+
+                <nav>
+                    <ul>
+                        { 
+                            NavbarLinks.map((link, index) => (
+                                <li key={index}>
+                                    {
+                                        link.title === "Catalog" ? (
+                                            <div>
+                                                <p>{link.title}</p>
+                                                <IOIosArrowDropdownCircle/>
+                                                <div>
+                                                    <div>
+
+                                                    </div>
+                                                    {
+                                                        subLinks.length ? (
+                                                            subLinks.map((subLinks, index) => (
+                                                                <Link to={`${subLinks.link}`} key={index}>
+                                                                    <p>{subLinks.title}</p>
+                                                                </Link>
+                                                            ))
+                                                        ) : (<div></div>)
+                                                    }
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <Link to={link?.path}>
+                                                <p className={`${ matchRoute(link?.path) ? "text-yellow-25" : "text-richblack-25"}`}>
+                                                    {link.title}
+                                                </p>
+                                            </Link>
+                                        )
+                                    }
+
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </nav>
+
+                {/* LogIn, Signup, Dashboard */}
+                <div className="flex gap-x-4 items-center">
+                    {
+                        user && user?.accountType != "Instructor" && (
+                            <Link to="/dashboard/cart" className="relative">
+                                <AiOutlineShoppingCart />
+                                {
+                                    totalItems > 0 && (
+                                        <span>
+                                            {totalItems}
+                                        </span>
+                                    )
+                                }
+                            </Link>
+                        )
+                    }
+                    {
+                        token === null && (
+                            <Link to="/login">
+                                <button className="border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md">
+                                    Log in
+                                </button>
+                            </Link>
+                        )
+                    }
+                    {
+                        token === null && (
+                            <Link to="/signup">
+                                <button className="border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md">
+                                    Sign UP
+                                </button>
+                            </Link>
+                        )
+                    }
+                    {
+                        token !== null && <ProfileDropDown />
+                    }
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
